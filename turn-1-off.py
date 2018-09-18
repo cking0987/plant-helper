b165814ceffb90263ddbc15d2a7434a221a446c7
Chuck King <cking@surveycto.com>
2018-09-17 22:44:37 -0400

    testing each relay with the pumps all connected

---
+++ turn-1-off.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
+
+# Set our GPIO numbering to BCM
+GPIO.setmode(GPIO.BCM)
+
+#set current pin location
+currentPin = 2
+
+#set mode and state to HIGH
+GPIO.setup(currentPin, GPIO.OUT)
+GPIO.output(currentPin, GPIO.HIGH)
+
+#turn it on
+GPIO.output(currentPin, GPIO.HIGH)
+print "relay at pin number " + currentPin " has been turned off"
